12a9f21019b2b781a171f068b9ed06ef3ef38ce1
xam-ileto <xam.ileto@gmail.com>
2022-05-30 17:11:42 +0800

    Added: user input and json formatting

--- udp_client.py
+++ udp_client.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 #Set variables for server address and destination port
 server_host='172.16.0.20' #IP address of the server CSNET01
@@ -6,16 +7,34 @@ dest_port=8009 #Input assigned port number for server 7
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 
-
-message = 'This is the message. It will be repeated.' 
 try:
     # TODO ask input from user
+    command = input("Input your command: ")
+    
+    username = input("Input your username: ")
+    
+    msg = input("Input your message (input anything if registering/deregistering): ")
     
     # TODO format as json
+    # if register/deregister
+    if (command != "msg"):
+        json_data = {
+            "command": command,
+            "username": username
+        }
+    else:
+        # if posting message
+        json_data = {
+            "command": command,
+            "username": username,
+            "message": msg
+        }
+    message = json.dumps(json_data)
     
     # Send data, string has to be sent as 'bytes' as string in Python is inUnicode format
     print('sending "%s"' % message)
     sent = sock.sendto(bytes(message,"utf-8"), (server_host,dest_port)) 
+    
     # waiting for server to send data back, this is blocking function
     print ('waiting to receive')
     data, server = sock.recvfrom(1024)
